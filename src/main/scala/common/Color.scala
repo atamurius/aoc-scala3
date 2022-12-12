f74f27f0e0f88bf27954a15a24a9faa69d5bca3a
Alexey Dsiuba <alexeyds@wix.com>
2022-12-12 14:02:01 +0200

    level display

--- src/main/scala/common/Color.scala
+++ src/main/scala/common/Color.scala
@@ -16,6 +16,17 @@ object Color:
   val blue = Color("34")
   val bright = Color("1")
   val gray = bright & black
-  
+
   val bgBlue = Color("44")
   val negative = Color("7")
+
+  val levels = {
+    val chars = ".:-=+*#%@"
+    chars.map(Color.blue(_)) ++
+      chars.map(Color.green(_)) ++
+      chars.map(Color.gray(_)) ++
+      chars.map(Color.white(_)) ++
+      chars.map(Color.white.and(_.bright)(_))
+  }
+
+  def displayLevel(value: Double): String = levels((value * (levels.size - 1)).toInt)
